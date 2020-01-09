7ccf4645c8e36db2c38d7d3da44473022c76e86f
yeongyumin <yeongyumin@vuno.co>
2020-01-09 11:14:04 +0900

    feat: add load ct slices and plot axial, sagittal and coronal images

---
+++ pydicom/load_ct_slices_plot_axial_sagittal_and_coronal_images.py
@@ -0,0 +1,62 @@
+import pydicom
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import glob
+
+files = []
+print('glob: {}'.format(sys.argv[1]))
+for fname in glob.glob(sys.argv[1], recursive=False):
+    print('loading: {}'.format(fname))
+    files.append(pydicom.read_file(fname))
+
+print("file count: {}".format(len(files)))
+
+# skip files with no SliceLocation (e.g scout views)
+slices = []
+skipcount = 0
+for f in files:
+    if hasattr(f, 'SliceLocation'):
+        slices.append(f)
+    else:
+        skipcount = skipcount + 1
+
+print("skipped, no SliceLocation: {}".format(skipcount))
+
+# ensure they are in correct order
+slices = sorted(slices, key=lambda s: s.SliceLocation)
+
+# pixel aspects, assuming all slices are the same
+ps = slices[0].PixelSpacing
+ss = slices[0].SliceThickness
+ax_aspect = ps[1]/ps[0]
+sag_aspect = ps[1]/ss
+cor_aspect = ss/ps[0]
+
+# create 3D array
+# 2D CT 이미지의 x, y 해상도
+img_shape = list(slices[0].pixel_array.shape)
+# 시계열로 3D CT 이미지를 만들기 위한 new img_shape 설정
+img_shape.append(len(slices))
+# 실제 3d 이미지 데이터를 구성하기 위한 numpy array
+img3d = np.zeros(img_shape)
+
+# fill 3D array with the images from the files
+for i, s in enumerate(slices):
+    img2d = s.pixel_array
+    img3d[:, :, i] = img2d
+
+# plot 3 orthogonal slices
+a1 = plt.subplot(2, 2, 1)
+plt.imshow(img3d[:, :, img_shape[2]//2])
+a1.set_aspect(ax_aspect)
+
+a2 = plt.subplot(2, 2, 2)
+plt.imshow(img3d[:, img_shape[1]//2, :])
+a2.set_aspect(sag_aspect)
+
+a3 = plt.subplot(2, 2, 3)
+plt.imshow(img3d[img_shape[1]//2, :, :].T)
+a3.set_aspect(cor_aspect)
+
+plt.show()
